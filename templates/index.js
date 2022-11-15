59698cc19b75a619528f79e17a57505e01cfabc9
Jin Bae <jinbae00@gmail.com>
2022-11-15 17:56:27 +0900

    방명록 필드 추가 밑 보기/닫기 기능 추가

--- templates/index.js
+++ templates/index.js
@@ -1,6 +1,13 @@
 // 방명록 버튼 누를시 입력 양식과 방명록 보이기. 버튼 다시 누르면 감추기
 function guestButton(){
-
+    let guestLog = document.getElementById("guestLog")
+    if (guestLog.style.display == 'none'){
+        $('#guestLog').show()
+        document.getElementById("showButton").innerHTML="방명록 닫기"
+    } else {
+        $('#guestLog').hide()
+        document.getElementById("showButton").innerHTML="방명록 보기/작성하기"
+    }
 }
 
 // Ajax와 연결해서 방명록 DB GET/POST 함수 만들기
