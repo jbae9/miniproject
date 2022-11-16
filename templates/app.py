cbbd64540c6c234ec39217215f5f65618ad66f81
98sw <118158994+98sw@users.noreply.github.com>
2022-11-16 20:58:39 +0900

    db설정

--- templates/app.py
+++ templates/app.py
@@ -1,22 +1,31 @@
 from flask import Flask, render_template, request, jsonify
-    app = Flask(__name__)
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.f3orwbt.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
-@app.route('/test', methods=['POST'])
-def test_post():
-    title_receive = request.form['title_give']
-    print(title_receive)
-    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+@app.route("/orders", methods=["POST"])
+def web_nameField_post():
+   name_receive = request.form['name_give']
+   comment_receive = request.form['comment_give']
 
-@app.route('/test', methods=['GET'])
-def test_get():
-    title_receive = request.args.get('title_give')
-    print(title_receive)
-    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+   doc = {
+         'name': name_recive,
+         'comment': comment_receive
+   }
+   db.orders.insert_one(doc)
+
+   return jsonify({'msg': '입력완료!'})
+
+@app.route("/mars", methods=["GET"])
+def web_nameField_get():
+   all_users = list(db.users.find({}, {'_id': False}))
+   return jsonify({'users':user_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-
+   app.run('0.0.0.0', port=5000, debug=True)
